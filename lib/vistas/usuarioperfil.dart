ff4a40a38edb8e32d1242d83ff0cb1132748bcdb
MrUshuario <lenonpoma@gmail.com>
2024-09-18 10:09:26 -0500

    login mejorado

---
+++ lib/vistas/usuarioperfil.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:barcelonaroom/obj/OBJapartamentos.dart';
+import 'package:barcelonaroom/utils/HelpersViewAlertaInfo.dart';
+import 'package:barcelonaroom/utils/resources.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class Usuario_perfil extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _Usuario_perfil();
+}
+
+class _Usuario_perfil extends State<Usuario_perfil> {
+
+  @override
+  void initState() {
+    funcion();
+    super.initState();
+
+  }
+
+  void funcion()  {
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true, //SACA LA BARRA DEBUG
+      home: Scaffold(
+        /*appBar: AppBar(
+          title: const Text(
+            Resources.usuario,
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color.fromARGB(255, 27, 65, 187),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.logout, color: Colors.white),
+                onPressed: () {
+                  logoutFunction();
+                }
+            )
+          ],
+        ), */
+        //drawer: const MenuLateral(),
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.asset(
+                Resources.backgroundAzul, // Replace with your image path
+                fit: BoxFit.cover, // Adjust fit as needed
+              ),
+            ),
+
+            // Existing content with Center, SingleChildScrollView, and Container
+            //Center( child:
+            SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  margin: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Form(
+                    //key: widget.keyForm,
+                    child: formUI(),
+                  ),
+                ),
+              ),
+           // ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget formUI() {
+    return Container(
+        child: Column(
+        children: [
+
+
+        ],),
+    );
+
+  }
+
+
+}
